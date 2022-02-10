121aa437c166848ff1eb690c8343714f21e32808
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-02-10 12:58:52 +1100

    tutorial: build ninja card (stateless)

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,84 @@ class NinjaCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          backgroundColor: Colors.grey[900],
           appBar: AppBar(
-        title: Text('Ninja ID Card'),
-      )),
+            title: Text('Ninja ID Card'),
+            centerTitle: true,
+            backgroundColor: Colors.grey[850],
+            elevation: 0,
+          ),
+          body: Padding(
+            padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/chunli.jpg'),
+                    radius: 50,
+                  ),
+                ),
+                Divider(
+                  height: 80,
+                  color: Colors.grey[600],
+                ),
+                Text(
+                  'Name'.toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'Chun-Li',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  'Current ninja level'.toUpperCase(),
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                    letterSpacing: 2,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  '8',
+                  style: TextStyle(
+                    color: Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.grey[400],
+                    ),
+                    SizedBox(width: 10),
+                    Text(
+                      'chun.li@gmail.com',
+                      style: TextStyle(
+                        color: Colors.grey[400],
+                        fontSize: 18,
+                        letterSpacing: 1,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )),
     );
   }
 }
